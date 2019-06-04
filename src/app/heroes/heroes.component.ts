c0ed2234bddc0f1cc034d901ccf33ea8b7669f5c
pepesan <pepesan@gmailcom>
2019-06-04 19:03:49 +0200

    mejoras de interfaz y nomenclatura

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,15 +15,29 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.hero = new Hero();
-    this.hero.name = 'Pepe';
+    this.hero.name = '';
     this.hero.id = 0;
     this.heroes = [];
-    this.heroes.push(new Hero(1, 'Juan'));
     // this.heroes.push(this.hero);
+    this.heroes.push(new Hero(1, 'Juan'));
+    // this.hero = new Hero(0, '');
   }
 
   addHero() {
     this.heroes.push(this.hero);
     this.hero = new Hero();
+  }
+
+  borraHeroe(h: Hero) {
+    this.heroes.splice(this.heroes.indexOf(h), 1);
+    /*
+    for ( let i = 0; i < this.heroes.length ; i++) {
+      const hero = this.heroes[i];
+      if ( h === hero) {
+        this.heroes.splice(i, 1);
+        break;
+      }
+    }
+     */
   }
 }
