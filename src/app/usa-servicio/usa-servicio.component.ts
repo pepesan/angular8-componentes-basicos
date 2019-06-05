ada93139faaddfe60ffff409348495610847cd94
pepesan <pepesan@gmailcom>
2019-06-05 18:35:23 +0200

    mejoras del ejemplo para meter una subscripción

--- src/app/usa-servicio/usa-servicio.component.ts
+++ src/app/usa-servicio/usa-servicio.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TodoService} from './todo.service';
+import {Observable} from 'rxjs';
+import {Partido} from '../entities/partido';
 
 @Component({
   selector: 'app-usa-servicio',
   templateUrl: './usa-servicio.component.html',
   styleUrls: ['./usa-servicio.component.css']
 })
-export class UsaServicioComponent implements OnInit {
-  partidos;
+export class UsaServicioComponent implements OnInit, OnDestroy {
+  partidos: Observable<Partido[]>;
+  partidosSinObserbable: Partido[] = [];
   constructor(private todoService: TodoService) {
     this.partidos = this.todoService.getData();
+    console.log(this.partidos);
+    this.partidos.subscribe((data) => {
+      console.log(data);
+      this.partidosSinObserbable = data;
+    },
+      error => {throw error});
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    // Aquí deberíamos desubcribirnos
   }
 
 }
